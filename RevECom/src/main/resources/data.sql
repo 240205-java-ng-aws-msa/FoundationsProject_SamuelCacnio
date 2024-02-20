e34a8a9c8c0a41de2eeba1225c66f661a8d3b655
KindOfThere <samcac2232@gmail.com>
2024-02-20 10:00:38 -0800

    moved images to proper folder

--- RevECom/src/main/resources/data.sql
+++ RevECom/src/main/resources/data.sql
@@ -18,7 +18,7 @@ create table product (
 insert into account values (1, 'jsmith@123', 'password');
 insert into account values (2, 'johndoe@99', 'password');
 
-insert into product values (1, 'Fury', "C:/Users/samca/Revature_Training/FoundationsProject_SamuelCacnio/RevECom/src/main/resources/static/fury.jpg", 60.0, 'M4 Sherman tank as seen in the eponymous film');
-insert into product values (2, 'KV-2', "C:/Users/samca/Revature_Training/FoundationsProject_SamuelCacnio/RevECom/src/main/resources/static/kv2.jpg", 80.0, 'The Death Fridge');
-insert into product values (3, 'Thunderbolt-VII', "C:/Users/samca/Revature_Training/FoundationsProject_SamuelCacnio/RevECom/src/main/resources/static/thunderboltvii.jpg", 70.0, 'Commanded by Lieutenant Colonel Creighton Abrams');
-insert into product values (4, 'Tiger-131', "C:/Users/samca/Revature_Training/FoundationsProject_SamuelCacnio/RevECom/src/main/resources/static/tiger131.jpg", 90.0, 'The last Tiger');
+insert into product values (1, 'Fury', 'C:/Users/samca/Revature_Training/FoundationsProject_SamuelCacnio/RevECom/src/main/resources/images/fury.jpg', 60.0, 'M4 Sherman tank as seen in the eponymous film');
+insert into product values (2, 'KV-2', 'C:/Users/samca/Revature_Training/FoundationsProject_SamuelCacnio/RevECom/src/main/resources/images/kv2.jpg', 80.0, 'The Death Fridge');
+insert into product values (3, 'Thunderbolt-VII', 'C:/Users/samca/Revature_Training/FoundationsProject_SamuelCacnio/RevECom/src/main/resources/images/thunderboltvii.jpg', 70.0, 'Commanded by Lieutenant Colonel Creighton Abrams');
+insert into product values (4, 'Tiger-131', 'C:/Users/samca/Revature_Training/FoundationsProject_SamuelCacnio/RevECom/src/main/resources/images/tiger131.jpg', 90.0, 'The last Tiger');
